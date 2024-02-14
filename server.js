97fa96d43049712cf2d171fd8079d3d1a309adb3
Anthony Jee <webdoostudio@gmail.com>
2024-02-15 07:26:48 +0800

    Revert server.js

--- server.js
+++ server.js
@@ -1,11 +1,67 @@
-// Set your secret key. Remember to switch to your live secret key in production.
-// See your keys here: https://dashboard.stripe.com/apikeys
-const stripe = require('stripe')('sk_test_51Jcqv9Gz9tfzSM8yRHgPIAPHPOT8y2iULtbHf0BayOEe9gM3TKGKfqCxeIiqaB9NFP5IG729eiqbKg0Z51HHMRxb00yokObx9p');
+const env = require("dotenv");
+env.config({ path: "./.env" });
+const express = require("express");
+const Stripe = require("stripe");
 
-const paymentIntent = await stripe.paymentIntents.create({
-  amount: 1099,
-  currency: 'usd',
-  payment_method_types: ['card'],
+const stripePublishableKey = process.env.STRIPE_PUBLISHABLE_KEY || "";
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY || "";
+
+
+const app = express();
+app.use(express.json())
+
+//Confirm the API version from your stripe dashboard
+const stripe = Stripe(stripeSecretKey, { apiVersion: "2023-10-16" });
+
+app.get("/get-publishable-key", async(req, res) => {
+  if(stripePublishableKey.length){
+    res.statusCode = (200)
+    res.json({publishableKeyFromServer: stripePublishableKey})
+  }
+  else{
+    res.statusCode = (503)
+    res.json({error: "Publishable key not available!"})
+  }
+})
+
+app.post("/create-payment-intent", async (req, res) => {
+  const {paymentMethodType, currency, amount} = req.body;
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Number(amount) * 100, //lowest denomination of particular currency
+      currency: currency,
+      payment_method_types: [paymentMethodType],
+      description: 'Software development services',
+      shipping: {
+        name: 'Jenny Rosen',
+        address: {
+          line1: '510 Townsend St',
+          postal_code: '98140',
+          city: 'San Francisco',
+          state: 'CA',
+          country: 'US',
+        },
+      },
+    });
+
+    const clientSecret = paymentIntent.client_secret;
+
+    res.json({
+      clientSecret: clientSecret,
+    });
+  } catch (e) {
+    console.log(e.message);
+    res.json({ error: e.message });
+  }
 });
-const clientSecret = paymentIntent.client_secret
-// Pass the client secret to the client
+
+app.all(/.*/, (req, res) => {
+  res.statusCode = (404)
+  res.send('Invalid Endpoint.')
+})
+
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+    console.log(`Server is listening at port number : ${PORT}`);
+})
