792306f6cfdbc68f8071667b063b2c0fd6002c4b
ANTHONYJEE <webdoostudio@gmail.com>
2024-03-26 23:20:46 +0800

    test stripe webhook

--- server.js
+++ server.js
@@ -10,6 +10,38 @@ const stripeSecretKey = process.env.STRIPE_SECRET_KEY || "";
 const app = express();
 app.use(express.json())
 
+
+
+// Webhook route
+app.post('/stripe-webhook', require('body-parser').raw({ type: '*/*' }),  async (req, res) => {
+    const sig = req.headers['stripe-signature'];
+    let event;
+
+    try {
+        event = stripe.webhooks.constructEvent(req.body, sig, 'whsec_LNaXRcnbkkAj3PionM4eT9piqXtaY93V');
+    } catch (err) {
+        console.error(`Webhook signature verification failed: ${err.message}`);
+        return res.sendStatus(400); // Bad request
+    }
+
+    // Handle the 'charge.succeeded' event specifically
+    if (event.type === 'charge.succeeded') {
+        const charge = event.data.object;
+        const receipt_url = charge.receipt_url;
+
+        if (receipt_url) {
+console.log('receipt_url': receipt_url);
+        } else {
+            console.error('No receipt_url found in the webhook event');
+        }
+    } 
+
+    res.sendStatus(200); // Successful response
+});
+
+
+
+
 //Confirm the API version from your stripe dashboard
 const stripe = Stripe(stripeSecretKey, { apiVersion: "2023-10-16" });
 
