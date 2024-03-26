d268a969732f960faf1b12f53219bf07374f5c33
ANTHONYJEE <webdoostudio@gmail.com>
2024-03-27 07:37:58 +0800

    retrieve receipt_url debug 3

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.get("/get-payment-details/:paymentId", async (req, res) => {
  } catch (e) {
    console.log(e.message);
    console.log(paymentId);
-   res.json({ error: paymentIntent });
+   res.json({ error: e.message });
    res.status(500).json({ error: "Failed to retrieve payment details" });
  }
 });
